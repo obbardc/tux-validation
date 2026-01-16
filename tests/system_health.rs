84645bfc8f0f5fd2bf88bf19cda31cebf5c482b6
Iakov Polyak <iakov.polyak@linaro.org>
2026-01-16 16:32:53 +0000

    Run cargo fmt --all to pass CI.

--- tests/system_health.rs
+++ tests/system_health.rs
@@ -4,6 +4,14 @@ use tux_validation::os_release;
 fn debian_and_forky() {
     let osr = os_release::parse_os_release("/etc/os-release").expect("Failed to read os-release");
 
-    assert_eq!(osr.get("ID").map(String::as_str), Some("debian"), "Not running Debian!");
-    assert_eq!(osr.get("VERSION_CODENAME").map(String::as_str), Some("forky"), "Codename is not `forky`!");
+    assert_eq!(
+        osr.get("ID").map(String::as_str),
+        Some("debian"),
+        "Not running Debian!"
+    );
+    assert_eq!(
+        osr.get("VERSION_CODENAME").map(String::as_str),
+        Some("forky"),
+        "Codename is not `forky`!"
+    );
 }
