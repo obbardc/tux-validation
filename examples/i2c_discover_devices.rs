68fe6231d296cf90e77f8bc07013dffa026d3091
Iakov Polyak <iakov.polyak@linaro.org>
2026-02-17 13:44:42 +0000

    Probe busses empty in udev too.

--- examples/i2c_discover_devices.rs
+++ examples/i2c_discover_devices.rs
@@ -43,34 +43,5 @@ fn main() -> anyhow::Result<()> {
         }
     }
 
-    //let reports = full_system_scan(args.hw_probe)?;
-    //for report in reports {
-    //    let sysfs_addrs: Vec<String> = report
-    //        .kernel_detected
-    //        .iter()
-    //        .map(|a| format!("0x{:02x}", a))
-    //        .collect();
-
-    //    let mut hw_unbound: Vec<String> = report
-    //        .hardware_unbound
-    //        .iter()
-    //        .map(|a| format!("U0x{:02x}", a))
-    //        .collect();
-
-    //    let mut hw_bound: Vec<String> = report
-    //        .hardware_bound
-    //        .iter()
-    //        .map(|a| format!("B0x{:02x}", a))
-    //        .collect();
-
-    //    hw_unbound.append(&mut hw_bound);
-
-    //    println!(
-    //        "{:<12} | {:<20} | {:<20}",
-    //        report.bus_path,
-    //        sysfs_addrs.join(", "),
-    //        hw_unbound.join(", ")
-    //    );
-    //}
     Ok(())
 }
